800e18b886787dacc15ca60eeb85b31dc7a505f8
Vitor Peçanha <vitor.pecanha@hotmail.com>
2024-10-08 22:53:51 -0300

    Added echo route

--- main.py
+++ main.py
@@ -25,5 +25,23 @@ def api_hello():
         return 'Hello stranger!'
 
 
+@app.route('/echo', methods=['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
+def api_echo():
+    if request.method == 'GET':
+        return "ECHO: GET\n"
+
+    elif request.method == 'POST':
+        return "ECHO: POST\n"
+
+    elif request.method == 'PATCH':
+        return "ECHO: PATCH\n"
+
+    elif request.method == 'PUT':
+        return "ECHO: PUT\n"
+
+    elif request.method == 'DELETE':
+        return "ECHO: DELETE\n"
+
+
 if __name__ == "__main__":
     app.run()
