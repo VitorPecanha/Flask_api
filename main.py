a0ebff7f44798aafe766d3258081c69c2e04d509
Vitor Peçanha <vitor.pecanha@hotmail.com>
2024-10-08 23:14:00 -0300

    Finished other methods

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, json, Response, jsonify
+from functools import wraps
 app = Flask(__name__)
 
 
@@ -41,6 +42,89 @@ def api_echo():
 
     elif request.method == 'DELETE':
         return "ECHO: DELETE\n"
+
+
+@app.route('/messages', methods=['POST'])
+def api_messages():
+    if request.headers['Content-Type'] == 'text/plain':
+        return 'Text message: ' + request.data
+
+    elif request.headers['Content-Type'] == 'application/jsons':
+        return 'Json Message: ' + json.dumps(request.json)
+
+    elif request.headers['Content-Type'] == 'application/octet-stream':
+        f = open('.binary', 'wb')
+        f.write(request.data)
+        f.close()
+        return "Binary message written!"
+
+    else:
+        return "415\nUnsupported Media Type"
+
+
+@app.route('/hello', methods=['GET'])
+def api_hello():
+    data = {
+        'hello': 'world',
+        'number': 3
+    }
+    js = json.dumps(data)
+
+    resp = Response(js, status=200, mimetype='application/json')
+    resp.headers['Link'] = 'http://luisrei.com'
+
+    return resp
+
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Not Found' + request.url,
+    }
+
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
+
+
+@app.route('/users/<userid>', methods=['GET'])
+def api_users(userid):
+    users = {'1': 'john', '2': 'steve', '3': 'bill'}
+
+    if userid in users:
+        return jsonify({userid: users[userid]})
+    else:
+        return not_found()
+
+
+def check_auth(username, password):
+    return username == 'admin' and password == 'secret'
+
+
+def authenticate():
+    message = {'message': 'Authenticate.'}
+    resp = jsonify(message)
+
+    resp.status_code = 401
+    resp.headers['WWW-Authenticate'] = 'Basic realm="Example"'
+
+    return resp
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth:
+            return authenticate()
+
+        elif not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    
+    return decorated()
 
 
 if __name__ == "__main__":
